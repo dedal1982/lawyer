c486fdb873ba72ea116a39021c1fbfe430b92775
aldm <d44.a1@yandex.ru>
2024-11-12 22:33:49 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const navLink = document.querySelectorAll(".promo__header-list li a");
+
+navLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    navLink.forEach((elem) => {
+      elem.classList.remove("active");
+      item.classList.add("active");
+    });
+  });
+});
