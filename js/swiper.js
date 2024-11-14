d94d71093749444202eaf77c3cdabc724f31b9c7
aldm <d44.a1@yandex.ru>
2024-11-14 16:38:38 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -19,4 +19,25 @@ const swiper = new Swiper(".services__swiper", {
     el: ".swiper-scrollbar",
   },
 });
-console.log(1111);
+
+const swiperTwo = new Swiper(".services__swiper-two", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
